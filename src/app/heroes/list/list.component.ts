14d90468311914c951dc83f2988af1641c788fc6
MartinDeveloper24 <developer24@grupotransforma.mx>
2023-11-15 13:13:44 -0600

    *ngIf-else y ng-template #

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  public heroNames: string[] = ['Mr. Nice', 'Narco', 'Bombasto', 'Celeritas', 'Magneta', 'RubberMan', 'Dynama', 'Dr IQ', 'Magma', 'Tornado'];
+  public deletedHero?: string;
 
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
